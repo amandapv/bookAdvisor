04a554829c71c4f607661366b7f8610cbf751d05
Amanda PV <amandaperezvazquez1@gmail.com>
2025-10-12 00:22:30 +0200

    move one line

--- src/main/java/com/example/bookAdvisor/HomeController.java
+++ src/main/java/com/example/bookAdvisor/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.bookAdvisor;
 
 import java.time.LocalDate;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
